08a81557320bdc0e73bdd09b5c8e84f152620832
Piyush Sao <piyush3@gatech.edu>
2015-12-16 14:46:40 -0500

    removed performance counters

--- sv.c
+++ sv.c
@@ -4,8 +4,9 @@
 #include <stdbool.h>
 #include <stdint.h>
 #include <inttypes.h>
+#include <malloc.h>
 
-bool SVSeq(size_t nv, uint32_t* component_map, uint32_t* off, uint32_t* ind) {
+bool BaselineSVSweep(size_t nv, uint32_t* component_map, uint32_t* off, uint32_t* ind) {
   bool changed = false;
 
   for (size_t v=0; v < nv; v++) {
@@ -21,48 +22,34 @@ bool SVSeq(size_t nv, uint32_t* component_map, uint32_t* off, uint32_t* ind) {
 	}
   }
 
+  /*shortcutting goes here*/
   return changed;
-  //shortcutting disabled
-/*  
- *  if (!changed)
-	return false;
 
-  for (size_t i = 0; i < nv; i++) {
-	while (component_map[i] != component_map[component_map[i]]) {
-	  component_map[i] = component_map[component_map[i]];
-	}
-  }
-
-  return true;
-*/
 }
 
-bool SVBranchless(size_t nv, uint32_t* component_map, uint32_t* off, uint32_t* ind) {
-  uint32_t changed = false;
 
-  for (size_t v=0; v < nv; v++) {
-	const uint32_t *restrict vind = &ind[off[v]];
-	const size_t vdeg = off[v + 1] - off[v];
 
-	for (size_t edge = 0; edge < vdeg; edge++){
-	  const uint32_t u = vind[edge];
-	  const uint32_t flag = (uint32_t)((int32_t)component_map[u]-(int32_t)component_map[v]) >> 31;
-	  component_map[v] += (-flag) & (component_map[u] - component_map[v]);
-	  changed += flag;
-	}
-  }
-   return changed;
-  //shortcutting disabled
-/*  
- *  if (!changed)
-	return false;
+uint32_t* BaselineSVMain( size_t numVertices, size_t numEdges, uint32_t* off, uint32_t* ind)
+{
+    
 
-  for (size_t i = 0; i < nv; i++) {
-	while (component_map[i] != component_map[component_map[i]]) {
-	  component_map[i] = component_map[component_map[i]];
-	}
-  }
+    uint32_t* components_map = (uint32_t*)memalign(64, numVertices * sizeof(uint32_t));
+	/* Initialize level array */
+    for (size_t i = 0; i < numVertices; i++)
+    {
+        components_map[i] = i;
+    }
 
-  return true;
-*/ 
+    bool changed;
+    size_t iteration = 0;
+    do
+    {
+		changed = BaselineSVSweep(numVertices, components_map, off, ind);
+		iteration += 1;
+    }
+    while (changed);
+
+    return components_map;
 }
+
+
