56e3e6f9d2f24b78b3ee9665f37bd8df31a776ec
Piyush Sao <piyush3@gatech.edu>
2016-01-23 20:18:35 -0500

    making time as double-2

--- stat.h
+++ stat.h
@@ -11,8 +11,8 @@ typedef struct
 {
 	/* data */
 	int numIteration;					/*number of iteration to converge*/
-	ltime SvTime[MAX_SV_SWEEP]; 		/*time for each SvItertation*/
-	ltime FtTime[MAX_SV_SWEEP]; 		/*time for each fault detection and correction*/
+	double SvTime[MAX_SV_SWEEP]; 		/*time for each SvItertation*/
+	double FtTime[MAX_SV_SWEEP]; 		/*time for each fault detection and correction*/
 	lcount SvMemCount[MAX_SV_SWEEP]; 		/*#memory access for each SvItertation*/
 	lcount FtMemCount[MAX_SV_SWEEP]; 		/*#memory access for each fault detection and correction*/
 
