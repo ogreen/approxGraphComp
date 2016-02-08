b0742c2ccde1b8c9f6dd3965d2d86e15a56d3c22
Piyush Sao <piyush3@gatech.edu>
2016-02-08 00:50:43 -0500

    Committing changes in paper so far

---
+++ paper/plots/plot_overhead_fault.m
@@ -0,0 +1,61 @@
+
+DT = csvread('Overhead-fault.csv');
+
+
+[m,n] =size(DT);
+
+linestyles = cellstr(char('-','-','-','-','-','-.','--','-',':','-',':',...
+'-.','--','-',':','-.','--','-',':','-.'));
+
+MarkerEdgeColors=hsv(n);  % n is the number of different items you have
+Markers=['o','s','d','v','^','<','>','p','h','.',...
+'+','*','o','x','*','x','^','<','h','+','.','>','p','s','d','v',...
+'o','x','+','*','s','d','v','^','<','>','p','h','.'];
+
+figure;
+for i = 1:n-1
+	
+	plot(DT(:,1),100*DT(:,i+1),[linestyles{i} Markers(i)],'Color',MarkerEdgeColors(i,:),'LineWidth',1.5);
+	hold on
+end
+
+
+set(gca,'XLim',[-18, -5]);
+% set(gca,'YLim',[-10, 400]);
+set(gca,'YLim',[0, 50]);
+
+hTitle  = title ('Overhead with increasing faults');
+hXLabel = xlabel('Fault injection rate'                     );
+hYLabel = ylabel('Relative overhead (in %)');    
+
+legend('astro-ph','CoPapersDBLP','kron\_g500-simple','cnr-2000','rgg\_2\_18');
+set(gcf, 'Color', 'w'); %setting the back ground color
+
+set(gca, 'XTickLabel',[])                      %# suppress current x-labels
+
+xt = get(gca, 'XTick');
+yl = get(gca, 'YLim');
+str = cellstr( num2str(xt(:),'2^{%d}') );      %# format x-ticks as 2^{xx}
+hTxt = text(xt, yl(ones(size(xt))), str, ...   %# create text at same locations
+    'Interpreter','tex', ...                   %# specify tex interpreter
+    'VerticalAlignment','top', ...             %# v-align to be underneath
+    'HorizontalAlignment','center');           %# h-aligh to be centered
+
+
+% set(0,'DefaultAxesColorOrder',[1 0 0;0 1 0;0 0 1;0.5 0.5 0;0.5 0 0.5; 0 0.5 0.5;0.5 0.5 0.25;0.5 0.25 0.5; 0.25 0.5 0.5;],...
+%       'DefaultAxesLineStyleOrder','-|--|:');
+% semilogy(Mres,'LineWidth',1.00);
+
+set(gca, ...
+  'Box'         , 'off'     , ...
+  'TickDir'     , 'out'     , ...
+  'TickLength'  , [.02 .02] , ...
+  'XMinorTick'  , 'off'      , ...
+  'YMinorTick'  , 'off'      , ...
+  'YGrid'       , 'on'      , ...
+  'XColor'      , [.3 .3 .3], ...
+  'YColor'      , [.3 .3 .3], ...
+  'LineWidth'   , 1         );
+
+% print2eps('plot_conv.eps'); %finally printing the files
+
