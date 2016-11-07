01609c0537d35ca8d334212e25a5e2c7bdfd6c5d
Srinivas Eswar <seswar3@gatech.edu>
2016-11-07 18:41:17 -0500

    Using subprocess.Popen to execute shell commands

--- ss_experiment/convergenceTestSync.py
+++ ss_experiment/convergenceTestSync.py
@@ -1,6 +1,7 @@
 #for different test matrices, runs the synchronous self-stabilizing iteration 
 #counts the number of iterations to converge.
 import sys
+import subprocess
 
 
 NumTrials = 100; #run for each case 100 times
@@ -10,11 +11,19 @@ faultRate = float(sys.argv[1])
 
 
 
-for i in xrange(1,NumTrials):
-	cmdstr = ('RSEED=%d PRINT_GRAPH=0 FAULT_PROB1=%f FAULT_PROB2=%f ../convTestSync'%(i, faultRate, faultRate)+sys.argv[2] 
-	# print(cmdstr)
-	f= sys.popen(str)
+for i in range(1,NumTrials):
+    cmdstr = 'RSEED=%d PRINT_GRAPH=0 FAULT_PROB1=%f FAULT_PROB2=%f ../convTestSync '%(i, faultRate, faultRate)+sys.argv[2] 
+    print(cmdstr)   
+    p = subprocess.Popen(cmdstr, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    op, err = p.communicate()
 
+    op = op.decode('utf-8')
+    op = op.strip()
+    op = op.replace("\n","")
 
+    err = err.decode('utf-8')
+    err = err.strip()
+    err = err.replace("\n","")
 
-	
+    print(op, err) 
+
