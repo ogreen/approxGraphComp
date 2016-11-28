1850a9dd1889783234432e6f1d05b847742b6a8e
Srinivas Eswar <seswar3@gatech.edu>
2016-11-28 17:34:42 -0500

    Adding new SVSweep for Triple Modular algorithm (removes two loop detection). Adding Triple Modular SV algorithm

--- ft_sv.c
+++ ft_sv.c
@@ -16,6 +16,7 @@
 #include <string.h>
 #include <errno.h>
 #include "sv.h"
+#include "sssv.h"
 
 
 
@@ -391,3 +392,268 @@ lp_state_t SCSVAlg_Sync( graph_t *graph,
     free_lp_state(&lps_prev);
     return lps_curr;
 }
+
+int  FISVSweep_Sync3(graph_t *graph, 
+                     lp_state_t* lp_state_in,      // State which is read in the iteration
+                     lp_state_t* lp_state_out,     // State which is written to in the iteration     
+                     uint32_t* FaultArrEdge,       /*probability of bit flip*/
+                     uint32_t* FaultArrCC )        /*probability of bit flip for type-2 faults*/
+{
+    size_t nv = graph->numVertices;
+    uint32_t* off = graph->off;
+    uint32_t* ind = graph->ind;
+    uint32_t* CC = lp_state_in->CC;
+
+    int  changed = 0;
+
+    // SV Sweep with faults
+    for (size_t v = 0; v < nv; v++)
+    {
+        uint32_t* vind = &ind[off[v]];
+        const size_t vdeg = off[v + 1] - off[v];
+
+        for (size_t edge = 0; edge < vdeg; edge++)
+        {
+            uint32_t uT = vind[edge];
+            uint32_t u;
+            MemAccessCount++;
+
+            if (FaultArrEdge[off[v] + edge])
+            {
+                /* code */
+                u = FaultInjectWord(uT);
+
+                while (u >= nv)    /*a better check can be used*/
+                {
+                    uT = vind[edge];
+                    MemAccessCount++;
+
+                    u = FaultInjectWord(uT);
+                    // printf("stuck 1\n");
+                }
+
+            }
+            else
+            {
+                u = uT;
+            }
+
+            uint32_t cc_prev_u = CC[u];
+            uint32_t var;
+            MemAccessCount++;
+            if (u == 0)
+            {
+                /* code */
+                cc_prev_u = 0;
+            }
+            else
+            {
+                if (FaultArrCC[off[v] + edge])
+                {
+                    var = FaultInjectWord(cc_prev_u);
+                    if ( var<cc_prev_u)
+
+                    {
+                        //printf("// Error injected %d %d\n",CC[u], var );
+                    }
+                }
+                else
+                {
+                    var = cc_prev_u;
+                }
+
+                cc_prev_u = var;
+            }
+
+            // Write to the auxiliary copy
+            if (cc_prev_u < lp_state_out->CC[v])
+            {
+                lp_state_out->CC[v] = cc_prev_u;
+                changed = 1;
+            }
+        }
+    }
+
+    /*shortcutting goes here*/
+    return changed;
+
+}
+int FISVModAlg_Sync( lp_state_t* lp_state_prev, graph_t *graph,
+                     stat_t* stat, int max_iter )
+// fault injected label propagation algorithm;
+// will give incorrect output (most likely)
+// used to measure failure rate
+// Correction based on Triple Modular Algorithm
+{
+    size_t numVertices  = graph->numVertices;
+    size_t numEdges  = graph->numEdges;
+    uint32_t* off  = graph->off;
+    uint32_t* ind  = graph->ind;
+
+    // Allocate auxillary state
+    lp_state_t lp_state_aux1, lp_state_aux2, lp_state_aux3, lp_state_aux4;
+    alloc_lp_state(graph, &lp_state_aux1);
+    init_lp_state(graph, &lp_state_aux1);
+    alloc_lp_state(graph, &lp_state_aux2);
+    init_lp_state(graph, &lp_state_aux2);
+    alloc_lp_state(graph, &lp_state_aux3);
+    init_lp_state(graph, &lp_state_aux3);
+
+    lp_state_t* lp_state_A = &lp_state_aux1;
+    lp_state_t* lp_state_B = &lp_state_aux2;
+    lp_state_t* lp_state_C = &lp_state_aux3;
+
+    alloc_lp_state(graph, &lp_state_aux4);
+    init_lp_state(graph, &lp_state_aux4);
+    lp_state_t* lp_state_cur = &lp_state_aux4;
+
+    /*get fault probability*/
+    double fProb1, fProb2;
+
+    getFault_prob(&fProb1, &fProb2);
+
+
+    uint32_t* AFaultArrEdge = (uint32_t*)memalign(64, numEdges * sizeof(uint32_t));
+    uint32_t* AFaultArrCC = (uint32_t*)memalign(64, numEdges * sizeof(uint32_t));
+    uint32_t* BFaultArrEdge = (uint32_t*)memalign(64, numEdges * sizeof(uint32_t));
+    uint32_t* BFaultArrCC = (uint32_t*)memalign(64, numEdges * sizeof(uint32_t));
+    uint32_t* CFaultArrEdge = (uint32_t*)memalign(64, numEdges * sizeof(uint32_t));
+    uint32_t* CFaultArrCC = (uint32_t*)memalign(64, numEdges * sizeof(uint32_t));
+
+
+    int  changed;
+    size_t iteration = 0;
+    int corrupted;
+    do
+    {
+        char label[100];
+        int votes_failed;        
+        int vote_iter = 1;
+        changed = 0;
+
+        do 
+        {
+            /*intialize fault array*/
+            for (int i = 0; i < numEdges; ++i)
+            {
+                /* code */
+                AFaultArrEdge[i] = 0;
+                AFaultArrCC[i] = 0;
+                BFaultArrEdge[i] = 0;
+                BFaultArrCC[i] = 0;
+                CFaultArrEdge[i] = 0;
+                CFaultArrCC[i] = 0;
+            }
+    
+            int numEdgeFault = 0;
+            int numCCFault = 0;
+    
+            while (numEdgeFault <  0.5 * fProb1 * numEdges)
+            {
+                uint32_t ind = rand() % numEdges;
+                AFaultArrEdge[ind] = 1;
+
+                ind = rand() % numEdges;
+                BFaultArrEdge[ind] = 1;
+
+                ind = rand() % numEdges;
+                CFaultArrEdge[ind] = 1;
+
+                numEdgeFault++;
+    
+            }
+    
+            while (numCCFault <  0.5 * fProb2 * numEdges)
+            {
+                uint32_t ind = rand() % numEdges;
+                AFaultArrCC[ind] = 1;
+
+                ind = rand() % numEdges;
+                BFaultArrCC[ind] = 1;
+
+                ind = rand() % numEdges;
+                CFaultArrCC[ind] = 1;
+
+                numCCFault++;
+    
+            }
+
+            corrupted = 0;
+            sprintf(label, "Iteration_%d", iteration);
+            printParentTree(label, graph, lp_state_prev);
+
+            votes_failed = 0;
+            
+            FISVSweep_Sync3(graph, lp_state_prev, lp_state_A, AFaultArrEdge, AFaultArrCC) ;
+            FISVSweep_Sync3(graph, lp_state_prev, lp_state_B, BFaultArrEdge, BFaultArrCC) ;
+            FISVSweep_Sync3(graph, lp_state_prev, lp_state_C, CFaultArrEdge, CFaultArrCC) ;
+
+            for (int i=0; i < numVertices && !votes_failed; i++)
+            {
+                if(lp_state_A->CC[i] == lp_state_B->CC[i])
+                {
+                    lp_state_cur->CC[i] = lp_state_A->CC[i];
+                }
+                else if(lp_state_A->CC[i] == lp_state_C->CC[i])
+                {
+                    lp_state_cur->CC[i] = lp_state_A->CC[i];
+                }
+                else if(lp_state_C->CC[i] == lp_state_B->CC[i])
+                {
+                    lp_state_cur->CC[i] = lp_state_C->CC[i];
+                }
+                else
+                {
+                    // Recount
+                    votes_failed = 1;
+                }
+            }
+            //printf("//Finished voting iteration %d\n", vote_iter);
+            vote_iter++;
+        }
+        while (votes_failed && vote_iter < max_iter);
+
+        if(vote_iter >= max_iter) 
+        {
+            printf("\nVotes couldn't agree within maximum allowed iterations");
+            iteration = max_iter;
+        }
+        iteration += 1;
+        sprintf(label, "Iteration_%d_after", iteration);
+        printParentTree(label, graph, lp_state_prev);
+
+        // Check convergence
+        for(int ii=0; ii < numVertices; ii++)
+            if(lp_state_prev->CC[ii] != lp_state_cur->CC[ii]) changed = 1;
+        
+        // Copy over current state to previous
+        memcpy(lp_state_prev->CC, lp_state_cur->CC, numVertices * sizeof(uint32_t));
+
+        // Copy over current state to A, B, C
+        memcpy(lp_state_A->CC, lp_state_cur->CC, numVertices * sizeof(uint32_t));
+        memcpy(lp_state_B->CC, lp_state_cur->CC, numVertices * sizeof(uint32_t));
+        memcpy(lp_state_C->CC, lp_state_cur->CC, numVertices * sizeof(uint32_t));
+
+        //printf("//Finished iteration  %d\n", iteration );
+    }
+    while ((changed || corrupted) && iteration<max_iter);
+
+    /*updating stats*/
+    stat->numIteration = iteration;
+    free (AFaultArrEdge);
+    free (AFaultArrCC);
+    free (BFaultArrEdge);
+    free (BFaultArrCC);
+    free (CFaultArrEdge);
+    free (CFaultArrCC);
+    printf("// Number of iteration is %d\n", iteration );
+
+    // Free LP aux
+    free_lp_state(lp_state_A);
+    free_lp_state(lp_state_B);
+    free_lp_state(lp_state_C);
+    free_lp_state(lp_state_cur);
+
+    return 0;
+}
+
